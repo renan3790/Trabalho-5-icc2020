b14218d61466cce3de106d4597eb545387cd590f
Renan Leandro <renanleandro2000@usp.br>
2020-06-29 19:09:50 +0000

    Contagem de jogadores.

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int main() {
+  printf("Bem vindo ao jogo Qwirkle.\nPara iniciar, insira a quantidade de jogadores e seus respectivos nomes.\n(Obs: limitado à um máximo de 18 jogadores.)\n");
+  int qtd;
+  scanf("%d", &qtd);
+  if (qtd>19||qtd<=0){
+    printf("Quantidade de jogadores inválida. Por favor, insira uma quantidade válida:\n")
+  }
+  else{
+    int x;
+    char jogador[x];
+    for(int i=0; i<x,i++){
+
+    }
+  }
 }
